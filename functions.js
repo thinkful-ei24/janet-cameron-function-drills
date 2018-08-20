4ced347455626a5eeff0cfc44031a56736950f48
Janet Leon <leon.irenejanet@gmail.com>
2018-08-20 12:57:42 -0700

    added toBeyond function

--- functions.js
+++ functions.js
@@ -1,5 +1,19 @@
+'use strict';
+
 function jediName(firstName, lastName){
-    return lastName.slice(0,3) + firstName.slice(0,2);
+  return lastName.slice(0,3) + firstName.slice(0,2);
 }
 
-console.log(jediName("Beyone", "Knowles"));
+console.log(jediName('Beyone', 'Knowles'));
+
+function toBeyond (num){
+  if (!isFinite(num)){
+    console.log('And beyond');
+  } else if (isFinite(num) && num >0 ) {
+    console.log('To infinity');
+  }else if (isFinite(num) && num <0 ) {
+    console.log('To negative infinity');
+  }else {console.log('Staying home');}
+}
+
+toBeyond(Infinity);
